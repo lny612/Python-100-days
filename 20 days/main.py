f9a0aaa41abbedb5fa6106f3e8f42c4cf8908471
lny612 <iris61214@gmail.com>
2022-04-30 18:19:59 +0900

    20-days: #1 Building the famous snake game

--- 20 days/main.py
+++ 20 days/main.py
@@ -6,22 +6,21 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
-screen.tracer(0) # until we call update, the screen will show nothing
+screen.tracer(0)  # until we call update, the screen will show nothing
 
 snake = Snake()
 
 screen.listen()
-screen.onkey(fun = snake.up, key = "w")
-screen.onkey(fun = snake.down, key = "s")
-screen.onkey(fun = snake.right, key = "d")
-screen.onkey(fun = snake.left, key = "a")
+screen.onkey(fun=snake.up, key="w")
+screen.onkey(fun=snake.down, key="s")
+screen.onkey(fun=snake.right, key="d")
+screen.onkey(fun=snake.left, key="a")
 
 game_is_on = True
 while game_is_on:
     screen.update()
-    time.sleep(0.1) # once the whole snake body finishes to move, I get my screen updated
+    time.sleep(0.1)  # once the whole snake body finishes to move, I get my screen updated
 
     snake.move()
-
 
 screen.exitonclick()
